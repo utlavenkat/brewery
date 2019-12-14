b3369f21f2a8a09f547b1c66b4d47e77b38f120e
Venkat Utla <venkatasubbaiah.utla@adp.com>
2019-12-14 13:43:00 +0530

    added customer service

---
+++ src/main/java/com/venkatutla/microservices/brewery/web/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.venkatutla.microservices.brewery.web.controller;
+
+import com.venkatutla.microservices.brewery.services.CustomerService;
+import com.venkatutla.microservices.brewery.web.dto.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping(path = "/{customerId}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable("customerId") UUID customerId) {
+        return new ResponseEntity(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/venkatutla/microservices/brewery/web/dto/Customer.java
@@ -0,0 +1,17 @@
+package com.venkatutla.microservices.brewery.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    private UUID id;
+    private String name;
+}
